155fcdbcbee22e0d2d99ac7d3819d9fc06eda9c2
Ami-007 <amivfletcher@gmail.com>
2021-12-06 17:40:57 +0000

    crreated component functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.scss';
+import Card from './components/Card/Card';
+import CardList from './components/CardList/CardList';
+import Main from './components/main/main';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
+const App = () => {
   return <p>Hello World!</p>
 }
 
