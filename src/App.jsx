3050afc394c6391c2b4a31954cc5564130f4ba5f
Ami-007 <amivfletcher@gmail.com>
2021-12-07 16:07:42 +0000

    added in bits to check if deployment works

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.scss';
-import Card from './components/Card/Card';
-import CardList from './components/CardList/CardList';
 import Main from './components/Main/Main';
 import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
-  return <p>Hello World!</p>
+  return (
+  <div>
+    <p>Hello World!</p>
+    <NavBar/>
+    <Main/>
+  </div>
+  )
+
 }
 
 export default App;
